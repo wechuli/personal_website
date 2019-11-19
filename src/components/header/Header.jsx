148dd56c8bf79fda746a579a3c17e07709325817
Wechuli <wechulipaul@yahoo.com>
2019-11-19 17:46:20 +0300

    header draft finish

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,32 @@ import "./Header.scss";
 const Header = () => {
   return (
     <header id="header">
-      <div className="container">This is the header</div>
+      <div className="container">
+        <div className="personal-details">
+          <div className="top">
+            <p>Hello, I'm</p>
+          </div>
+          <div className="name">Wechuli Paul</div>
+          <div className="title">
+            <p>Software Developer and Cloud Architect</p>
+          </div>
+          <div className="contact-icons">
+            <i class="fab fa-github fa-2x"></i>
+            <i class="fab fa-linkedin fa-2x"></i>
+            <i class="fab fa-twitter fa-2x"></i>
+            <i class="far fa-envelope fa-2x"></i>
+          </div>
+          <div className="cta-buttons">
+            <a href="" className="btn-main">
+              Contact Me
+            </a>
+
+            <a href="" className="btn-light">
+              See My Resume
+            </a>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
---
+++ src/components/project-showcase/ProjectShowCase.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ProjectShowCase.scss';
+
+const ProjectShowCase = () => {
+    return (
+
+        <section className="project-showcase">
+            <div className="container">
+                this is the showcase
+            </div>
+        </section>
+    )
+}
+
+export default ProjectShowCase
